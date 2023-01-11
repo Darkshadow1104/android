a28a395bdedaa0f02a6d472b4768e170c6a4300e
Nitesh Narwade <89506886+Darkshadow1104@users.noreply.github.com>
2023-01-11 22:50:33 +0530

    Update AutoFitTextureView.java

--- app/src/main/java/Cosylab/tensorflow/lite/examples/detection/customview/AutoFitTextureView.java
+++ app/src/main/java/Cosylab/tensorflow/lite/examples/detection/customview/AutoFitTextureView.java
@@ -1,18 +1,4 @@
-/*
- * Copyright 2019 The TensorFlow Authors. All Rights Reserved.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *       http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
+
 
 package Cosylab.tensorflow.lite.examples.detection.customview;
 
@@ -37,14 +23,7 @@ public class AutoFitTextureView extends TextureView {
     super(context, attrs, defStyle);
   }
 
-  /**
-   * Sets the aspect ratio for this view. The size of the view will be measured based on the ratio
-   * calculated from the parameters. Note that the actual sizes of parameters don't matter, that is,
-   * calling setAspectRatio(2, 3) and setAspectRatio(4, 6) make the same result.
-   *
-   * @param width Relative horizontal size
-   * @param height Relative vertical size
-   */
+  
   public void setAspectRatio(final int width, final int height) {
     if (width < 0 || height < 0) {
       throw new IllegalArgumentException("Size cannot be negative.");
