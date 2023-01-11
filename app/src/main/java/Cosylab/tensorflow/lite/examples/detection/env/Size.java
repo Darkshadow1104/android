afadf87d83765599d20f9d1944911c57146d15b3
Nitesh Narwade <89506886+Darkshadow1104@users.noreply.github.com>
2023-01-11 22:43:54 +0530

    Update Size.java

--- app/src/main/java/Cosylab/tensorflow/lite/examples/detection/env/Size.java
+++ app/src/main/java/Cosylab/tensorflow/lite/examples/detection/env/Size.java
@@ -1,17 +1,4 @@
-/* Copyright 2019 The TensorFlow Authors. All Rights Reserved.
 
-Licensed under the Apache License, Version 2.0 (the "License");
-you may not use this file except in compliance with the License.
-You may obtain a copy of the License at
-
-    http://www.apache.org/licenses/LICENSE-2.0
-
-Unless required by applicable law or agreed to in writing, software
-distributed under the License is distributed on an "AS IS" BASIS,
-WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-See the License for the specific language governing permissions and
-limitations under the License.
-==============================================================================*/
 
 package Cosylab.tensorflow.lite.examples.detection.env;
 
@@ -41,13 +28,7 @@ public class Size implements Comparable<Size>, Serializable {
     this.height = bmp.getHeight();
   }
 
-  /**
-   * Rotate a size by the given number of degrees.
-   *
-   * @param size Size to rotate.
-   * @param rotation Degrees {0, 90, 180, 270} to rotate the size.
-   * @return Rotated size.
-   */
+ 
   public static Size getRotatedSize(final Size size, final int rotation) {
     if (rotation % 180 != 0) {
       // The phone is portrait, therefore the camera is sideways and frame should be rotated.
