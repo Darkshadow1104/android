c4c899e0d9fd6917dbe34c135477a1bffd88929c
Nitesh Narwade <89506886+Darkshadow1104@users.noreply.github.com>
2023-01-11 22:47:13 +0530

    Update ImageUtils.java

--- app/src/main/java/Cosylab/tensorflow/lite/examples/detection/env/ImageUtils.java
+++ app/src/main/java/Cosylab/tensorflow/lite/examples/detection/env/ImageUtils.java
@@ -1,17 +1,4 @@
-/* Copyright 2019 The TensorFlow Authors. All Rights Reserved.
 
-Licensed under the Apache License, Version 2.0 (the "License");
-you may not use this file except in compliance with the License.
-You may obtain a copy of the License at
-
-    http://www.apache.org/licenses/LICENSE-2.0
-
-Unless required by applicable law or agreed to in writing, software
-distributed under the License is distributed on an "AS IS" BASIS,
-WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-See the License for the specific language governing permissions and
-limitations under the License.
-==============================================================================*/
 
 package Cosylab.tensorflow.lite.examples.detection.env;
 
@@ -30,10 +17,7 @@ public class ImageUtils {
   @SuppressWarnings("unused")
   private static final Logger LOGGER = new Logger();
 
-  /**
-   * Utility method to compute the allocated size in bytes of a YUV420SP image of the given
-   * dimensions.
-   */
+ 
   public static int getYUVByteSize(final int width, final int height) {
     // The luminance plane requires 1 byte per pixel.
     final int ySize = width * height;
@@ -45,21 +29,12 @@ public class ImageUtils {
     return ySize + uvSize;
   }
 
-  /**
-   * Saves a Bitmap object to disk for analysis.
-   *
-   * @param bitmap The bitmap to save.
-   */
+ 
   public static void saveBitmap(final Bitmap bitmap) {
     saveBitmap(bitmap, "preview.png");
   }
 
-  /**
-   * Saves a Bitmap object to disk for analysis.
-   *
-   * @param bitmap The bitmap to save.
-   * @param filename The location to save the bitmap to.
-   */
+  
   public static void saveBitmap(final Bitmap bitmap, final String filename) {
     final String root =
         Environment.getExternalStorageDirectory().getAbsolutePath() + File.separator + "tensorflow";
@@ -151,20 +126,7 @@ public class ImageUtils {
     }
   }
 
-  /**
-   * Returns a transformation matrix from one reference frame into another. Handles cropping (if
-   * maintaining aspect ratio is desired) and rotation.
-   *
-   * @param srcWidth Width of source frame.
-   * @param srcHeight Height of source frame.
-   * @param dstWidth Width of destination frame.
-   * @param dstHeight Height of destination frame.
-   * @param applyRotation Amount of rotation to apply from one frame to another. Must be a multiple
-   *     of 90.
-   * @param maintainAspectRatio If true, will ensure that scaling in x and y remains constant,
-   *     cropping the image if necessary.
-   * @return The transformation fulfilling the desired requirements.
-   */
+ 
   public static Matrix getTransformationMatrix(
       final int srcWidth,
       final int srcHeight,
